3abc0a8a50094da7a487d449f5f2ffdb4ba8fc97
TOMOQ1024 <53770396+TOMOQ1024@users.noreply.github.com>
2022-08-05 16:56:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let cvs, _cvs, ctx, _ctx, imgData, _imgData, pixels, _pixels, w, h, itvl;
-      
+
 window.addEventListener("load", ()=>{
   cvs = document.querySelector("canvas");
   _cvs = document.createElement("canvas");
@@ -14,9 +14,9 @@ window.addEventListener("load", ()=>{
   imgData = ctx.createImageData(w, h);
   pixels = imgData.data;
   //document.body.append(cvs);
-        
+
   pixels.forEach((v,i)=>{if(i%4==3)pixels[i]=255});
-        
+
   (function(){
     _ctx.font = `bold ${Math.min(w/3,h/4)}px gothic`;
     _ctx.textAlign = "center";
@@ -28,55 +28,72 @@ window.addEventListener("load", ()=>{
     _ctx.fillText("三原色", w/2, h/4);
     _ctx.globalCompositeOperation = "lighter";
     _ctx.fillStyle = "#f00";
+    
+    let r = Math.min(w/4,h/5);
     _ctx.beginPath();
-    _ctx.arc(w/2, h/2, w/4, 0, Math.PI*2);
+    _ctx.arc(
+      w*.5-r/2*Math.sin(Math.PI/3*0),
+      h*.6-r/2*Math.cos(Math.PI/3*0),
+      r, 0, Math.PI*2);
     _ctx.fill();
     _ctx.fillStyle = "#0f0";
     _ctx.beginPath();
-    _ctx.arc(w/3, h*2/3, w/4, 0, Math.PI*2);
+    _ctx.arc(
+      w*.5-r/2*Math.sin(Math.PI/3*2),
+      h*.6-r/2*Math.cos(Math.PI/3*2),
+      r, 0, Math.PI*2);
     _ctx.fill();
     _ctx.fillStyle = "#00f";
     _ctx.beginPath();
-    _ctx.arc(w*2/3, h*2/3, w/4, 0, Math.PI*2);
+    _ctx.arc(
+      w*.5-r/2*Math.sin(Math.PI/3*4),
+      h*.6-r/2*Math.cos(Math.PI/3*4),
+      r, 0, Math.PI*2);
     _ctx.fill();
     _imgData = _ctx.getImageData(0, 0, w, h);
     _pixels = _imgData.data;
-    
-    let i;
-     for(let y=0; y<h; ++y){
-       for(let x=0; x<w; ++x){
-         i = x*4+y*w*4;
-         if(!_pixels[i+3]){
-           pixels[i+0] = (x+3)%3 ? 0 : 255;
-           pixels[i+1] = (x+2)%3 ? 0 : 255;
-           pixels[i+2] = (x+1)%3 ? 0 : 255;
-         }
-         else{
-           pixels[i+0] = (x+3)%3 ? 0 : _pixels[i+0];
-           pixels[i+1] = (x+2)%3 ? 0 : _pixels[i+1];
-           pixels[i+2] = (x+1)%3 ? 0 : _pixels[i+2];
-         }
-       }
-     }
+
+    let i, j;
+    for(let y=0; y<h; ++y){
+      for(let x=0; x<w; ++x){
+        i = x*4+y*w*4;
+        if(!_pixels[i+3]){
+          pixels[i+0] = (x+3)%3 ? 0 : 255;
+          pixels[i+1] = (x+2)%3 ? 0 : 255;
+          pixels[i+2] = (x+1)%3 ? 0 : 255;
+        }
+        else{
+          pixels[i+0] = (x+3)%3 ? 0 : _pixels[i+0];
+          pixels[i+1] = (x+2)%3 ? 0 : _pixels[i+1];
+          pixels[i+2] = (x+1)%3 ? 0 : _pixels[i+2];
+        }
+      }
+    }
     ctx.putImageData(imgData, 0, 0);
   }());
-        
-        
-  cvs.addEventListener("click", ()=>{
-    return;
+
+
+  cvs.addEventListener("click", (e)=>{
     d=0;
+    let mx = e.clientX;
+    let my = e.clientY;
     if(itvl)clearInterval(itvl);
     itvl = setInterval(()=>{
       for(let y=0; y<h; ++y){
         for(let x=0; x<w; ++x){
           i = x*4+y*w*4;
-          pixels[i+0] = (x+3)%3 ? 0 : _pixels[i+0];
-          pixels[i+1] = (x+2)%3 ? 0 : _pixels[i+1];
-          pixels[i+2] = (x+1)%3 ? 0 : _pixels[i+2];
-          break;
-          pixels[i+0] = (Math.floor(x/(d**3+1))+3)%3 ? 0 : 255;
-          pixels[i+1] = (Math.floor(x/(d**3+1))+2)%3 ? 0 : 255;
-          pixels[i+2] = (Math.floor(x/(d**3+1))+1)%3 ? 0 : 255;
+          j = Math.floor(Math.min((x-mx)/(d**3+1)+mx,w-1))*4
+            + Math.floor(Math.min((y-my)/(d**3+1)+my,h-1))*w*4;
+          if(!_pixels[j+3]){
+            pixels[i+0] = (Math.floor(x/(d**3+1))+3)%3 ? 0 : 255;
+            pixels[i+1] = (Math.floor(x/(d**3+1))+2)%3 ? 0 : 255;
+            pixels[i+2] = (Math.floor(x/(d**3+1))+1)%3 ? 0 : 255;
+          }
+          else{
+            pixels[i+0] = (Math.floor(x/(d**3+1))+3)%3 ? 0 : _pixels[j+0];
+            pixels[i+1] = (Math.floor(x/(d**3+1))+2)%3 ? 0 : _pixels[j+1];
+            pixels[i+2] = (Math.floor(x/(d**3+1))+1)%3 ? 0 : _pixels[j+2];
+          }
         }
       }
 
